efa8a5c3c5639857a2292a8989cdfa3e4a97f500
Shivam-0411 <shivamchoudhary041103@gmail.com>
2024-11-13 10:11:06 +0530

    Commit 2 on 13 Nov

--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -1,24 +1,34 @@
 const user = require("../models/user");
-const otpgenerator = require("otp-generator");
+const otpGenerator = require("otp-generator");
+
 //send OTP
-module.exports = async(req, res) => {
-    const {email} = req.body;
+exports.sendOTP = async(req, res) => {
+    try {
+        const {email} = req.body;
 
-    //check if mail already exists or not
-    const isMailPresent = await user.findOne({email});
+        //check if mail already exists or not
+        const isMailPresent = await user.findOne({email});
 
-    //if mail present, return with false...if not present, continue
-    if(isMailPresent) {
-        res.status(401).json({
-            success: false,
-            message: "User already exists"
+        //if mail present, return with false...if not present, continue
+        if(isMailPresent) {
+            res.status(401).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+
+        var otp = otpGenerator.generate(6, {
+            
         })
+    } catch (error) {
+        
     }
-
-    const otp = otpgenerator
 }
 
 //signup 
+exports.signUp = async(req, res) => {
+
+}
 
 //login
 
